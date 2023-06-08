1a175e41a5c5a7f0aabe6d880383a04de8067a6e
andalusia238 <andalusia238@gmail.com>
2023-06-07 23:46:53 -0400

    starting ejs stuff and the stuff before it

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 // set up the server
 const DEBUG = true;
+const db = require('./db/db_connection');
 const express = require("express" );
 const app = express()
 const logger = require("morgan");
 const port = 3000;
+
+// Configure Express to use EJS
+app.set( "views",  __dirname + "/views");
+app.set( "view engine", "ejs" );
 
 //start the server
 app.listen(port, () => {
@@ -22,14 +27,54 @@ app.get( "/", ( req, res ) => {
 } );
 
 // define a route for the costume list page
+const read_costume_list_all_sql = `
+    SELECT 
+        costume_id, name, size, image 
+    FROM costume
+    ORDER BY name
+`
 app.get( "/costume_list", ( req, res ) => {
-    res.sendFile( __dirname + "/views/costume_list.html" );
-} );
+    db.execute(read_costume_list_all_sql, (error, results) => {
+        if (DEBUG)
+            console.log(error ? error : results);
+        if (error)
+            res.status(500).send(error); //Internal Server Error
+        else
+            res.send(results);
+    });
+});
 
-//define a route for the costume detail page
-app.get( "/detail", ( req, res ) => {
-    res.sendFile( __dirname + "/views/detail.html" );
-} );
+//route to send back static html page
+// app.get( "/costume_list", ( req, res ) => {
+//     res.sendFile( __dirname + "/views/costume_list.html" );
+// } );
+
+
+// define a route for the costume detail page
+const read_costume_detail_sql = `
+    SELECT
+        costume_id, name, size, image, description
+    FROM costume
+    WHERE costume_id = ?
+`
+app.get( "/:id", ( req, res ) => {
+    db.execute(read_costume_detail_sql, [req.params.id], (error, results) => {
+        if (DEBUG)
+            console.log(error ? error : results);
+        if (error)
+            res.status(500).send(error); //Internal Server Error
+        else if (results.length == 0)
+            res.status(404).send(`No costume id found with id = "${req.params.id}"` ); // NOT FOUND
+        else
+            res.send(results[0]); // results is still an array
+    });
+});
+
+
+//define a route for the costume detail page -> OLDD!!!
+// app.get( "/detail", ( req, res ) => {
+//     res.sendFile( __dirname + "/views/detail.html" );
+// } );
 
 // define a route for the assignment detail page
 // app.get("/assignments/details", (req, res) => {
