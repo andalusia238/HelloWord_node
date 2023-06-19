063549fe58b1d76085a307bb2752b0cce4bc8863
andalusia238 <andalusia238@gmail.com>
2023-06-19 17:16:01 -0400

    removed cooking project files to other account

--- app.js
+++ app.js
@@ -28,56 +28,126 @@ app.get( "/", ( req, res ) => {
     res.render('index');
 });
 
-
-//TESTING FOR COOKING WEB
-// define a route for the default home page
-app.get( "/cookingweb", ( req, res ) => {
-    res.render('CW_index.ejs');
-});
-
-app.post( "/search", ( req, res) => {
-    const ingredients = [];
-    for (let i=1; i<=5; i++) {
-        const ingredient = req.body[`ingredient${i}`];
-        if (ingredient) {
-            ingredients.push(ingredient)
-        }
-    }
-
-
-// my sql query
-// getting the number of items in the array
-const ingredientNum = ingredients.length;
-const ingredientList = ingredients.map(() => '?').join(',');
-const search_database_for_ingredients_sql = `
-SELECT recipe.recipe_name
-FROM recipe
-JOIN recipe_ingredient_xref
-    ON recipe.recipe_id = recipe_ingredient_xref.recipe_id
-JOIN ingredient
-    ON recipe_ingredient_xref.ingredient_id = ingredient.ingredient_id
-WHERE ingredient_name IN (${ingredientList})
-GROUP BY recipe.recipe_id, recipe.recipe_name
-HAVING COUNT(DISTINCT ingredient_name) = ?
-LIMIT 20
+// define a route for the costume list page
+const read_costume_list_all_sql = `
+    SELECT 
+        costume_id, name, size, image 
+    FROM costume
+    ORDER BY name
 `
-const finalArray = ingredients.concat(ingredientNum);
-//const params = [...ingredients, ingredientNum];
-
-db.execute(search_database_for_ingredients_sql, finalArray, (error, results) => {
+app.get( "/costume_list", ( req, res ) => {
+    db.execute(read_costume_list_all_sql, (error, results) => {
         if (DEBUG)
             console.log(error ? error : results);
         if (error)
             res.status(500).send(error); //Internal Server Error
         else {
-            //let data = {searchresult : results };
-            //res.render('CW_results', data);
-            res.render('CW_results.ejs', { results });
+            let data = {costumeList : results };
+            res.render('costume_list', data);
         }
     });
 });
 
-// define a route for the default home page
-//app.get( "/cookingweb/results", ( req, res ) => {
-//    res.render('CW_results.ejs');
-//});
+//route to send back static html page
+// app.get( "/costume_list", ( req, res ) => {
+//     res.sendFile( __dirname + "/views/costume_list.html" );
+// } );
+
+
+// define a route for the costume detail page
+const read_costume_detail_sql = `
+    SELECT
+        costume_id, name, size, image, description
+    FROM costume
+    WHERE costume_id = ?
+`
+app.get( "/:id", ( req, res ) => {
+    db.execute(read_costume_detail_sql, [req.params.id], (error, results) => {
+        if (DEBUG)
+            console.log(error ? error : results);
+        if (error)
+            res.status(500).send(error); //Internal Server Error
+        else if (results.length == 0)
+            res.status(404).send(`No costume id found with id = "${req.params.id}"` ); // NOT FOUND
+        else {
+            let data = {costume: results[0]};
+            res.render('detail', data); 
+        }
+            
+    });
+});
+
+
+// define a route for costume DELETE
+// YOU CAN ONLY DELETE COSTUME ID's 1,2,&4.  COSTUME ID 3 WHICH IS THE WITCH IS CONNECTED TO A SAMPLE ORDER
+// CAN I RUN 2 QUERIES INSIDE HERE?  FIRST TO DELETE ANY ORDER ATTACHED TO A COSTUME AND THEN SECOND TO DELETE THE COSTUME RECORD?
+const delete_costume_sql = `
+    DELETE 
+    FROM
+        costume
+    WHERE
+        costume_id = ?
+
+`
+
+app.get("/:id/delete", ( req, res ) => {
+    db.execute(delete_costume_sql, [req.params.id], (error, results) => {
+        if (DEBUG)
+            console.log(error ? error : results);
+        if (error)
+            res.status(500).send(error); //Internal Server Error
+        else {
+            res.redirect("/costume_list");
+        }
+    });
+});
+
+// define a route for costume CREATE
+// NOTE HAD TO PRESET USER ID = 1 BECAUSE COSTUMES WILL NORMALLY BE CONNECTED TO A USER, BUT I HAVE NOT COMPLETE THAT PART OF THE WEB APP YET
+// HOW TO UPLOAD AN IMAGE - I AM ABLE TO STORE THE FILE NAME IN THE DB, BUT HOW TO GET THE IMAGE UPLOADED
+const create_costume_sql = `
+    INSERT INTO costume 
+        (name, size, description, image, user_id) 
+    VALUES 
+        (?, ?, ?, ?, 1);
+`
+app.post("/costume_list", ( req, res ) => {
+    db.execute(create_costume_sql, [req.body.name, req.body.size, req.body.description, req.body.image], (error, results) => {
+        if (DEBUG)
+            console.log(error ? error : results);
+        if (error)
+            res.status(500).send(error); //Internal Server Error
+        else {
+            res.redirect(`/costume_list`);
+        }
+    });
+});
+
+
+// define a route for assignment UPDATE
+const update_costume_sql = `
+    UPDATE
+        costume
+    SET
+        name = ?,
+        size = ?,
+        description = ?,
+        image = ?
+    WHERE
+        costume_id = ?
+`
+app.post("/:id", ( req, res ) => {
+    db.execute(update_costume_sql, [req.body.name, req.body.size, req.body.description, req.body.image, req.params.id], (error, results) => {
+        if (DEBUG)
+            console.log(error ? error : results);
+        if (error)
+            res.status(500).send(error); //Internal Server Error
+        else {
+            res.redirect(`/${req.params.id}`);
+        }
+    });
+});
+
+
+
+
